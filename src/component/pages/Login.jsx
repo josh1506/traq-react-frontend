b25d3fc5fa7e37243b5b4ad1586ed0ce2dff9a83
josh1506 <jmjabor@gmail.com>
2021-04-12 16:41:14 +0800

    Add Facebook login button

--- src/component/pages/Login.jsx
+++ src/component/pages/Login.jsx
@@ -1,20 +1,34 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import FacebookLogin from 'react-facebook-login';
 
 import img_one from '../assets/img/11000.jpg'
 import logo from '../assets/img/Traq_Logo.png'
 import '../assets/css/pages/login.css'
 
 function Login(props) {
-    return (
-        <div>
-            <img src={img_one} alt="" height='200' />
-            <div>
-                <img src={logo} alt='Traq_Logo' height='100' />
-                <p>
+    const responseFacebook = (response) => {
+        console.log(response);
+    }
 
+    return (
+        <div className='login-page-container'>
+            <img src={img_one} alt="" className='login-img' />
+            <div className='login-container'>
+                <img src={logo} alt='Traq_Logo' />
+                <h1>Welcome to Traq</h1>
+                <p>
+                    Sign in now to assess and start tracking your audience.
                 </p>
                 <h3>Login/Signup with:</h3>
-                <button>F</button>
+                <FacebookLogin
+                    appId="199680755295170"
+                    fields="name,email,picture"
+                    callback={responseFacebook}
+                    cssClass="facebook-login-container"
+                    textButton={<FontAwesomeIcon icon={faFacebookF} color='white' size='3x' className='facebook-button' />}
+                />
             </div>
         </div>
     );
