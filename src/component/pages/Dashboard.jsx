b701d088c033acc91025ae40f9c2d20d1061e66c
josh1506 <jmjabor@gmail.com>
2021-04-12 12:34:10 +0800

    Create Dashboard.jsx

---
+++ src/component/pages/Dashboard.jsx
@@ -0,0 +1,39 @@
+import React from 'react';
+
+import '../assets/css/pages/dashboard.css'
+
+function Dashboard(props) {
+    return (
+        <div>
+            <div>
+                <div></div>
+                <div>
+                    graph
+                </div>
+            </div>
+            <input type="text" name="" id="" />
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Title</th>
+                            <th>Link</th>
+                            <th>Total Visitors</th>
+                            <th>Last Viewed</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                            <td></td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    );
+}
+
+export default Dashboard;
