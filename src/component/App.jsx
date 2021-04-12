bf6d1bec796dde5c72bcd0aac2728e03ce5220bf
josh1506 <jmjabor@gmail.com>
2021-04-12 12:33:47 +0800

    Add routes

---
+++ src/component/App.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom'
+
+import Dashboard from './pages/Dashboard';
+import Details from './pages/Details';
+import Landing from './pages/Landing';
+import Login from './pages/Login';
+import Nav from './common/Nav';
+import './assets/css/app.css'
+
+function App(props) {
+    return (
+        <div>
+            <Nav />
+            <main>
+                <Switch>
+                    <Route path='/login' component={Login} />
+                    <Route path='/dashboard/:id' component={Details} />
+                    <Route path='/dashboard' component={Dashboard} />
+                    <Route path='/' component={Landing} />
+                </Switch>
+            </main>
+        </div>
+    );
+}
+
+export default App;
