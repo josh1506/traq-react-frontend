8f8f863d767e4d4629e305722828ba59efd3161a
josh1506 <jmjabor@gmail.com>
2021-04-14 12:04:12 +0800

    Update CreateUrlModal.jsx

--- src/component/common/CreateUrlModal.jsx
+++ src/component/common/CreateUrlModal.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { motion } from 'framer-motion'
+import { motion, AnimatePresence } from 'framer-motion'
 
 import '../assets/css/common/modal.css'
 
@@ -40,14 +40,17 @@ const animateFormContainer = {
 
 const animateModalBackground = {
     hidden: {
+        zIndex: 1,
         opacity: 0
     },
     visible: {
+        zIndex: 1,
         opacity: 1,
     },
     exit: {
-        zIndex: -1,
-        opacity: 0
+        zIndex: 1,
+        opacity: 0,
+        transition: { delay: 0.2 }
     },
 }
 
@@ -70,50 +73,52 @@ const animateModalContainer = {
 
 function CreateUrlModal(props) {
     return (
-        <motion.div
-            variants={animateModalBackground}
-            initial='hidden'
-            animate='visible'
-            exit='exit'
-            className='modal-background'>
-            <div style={{ display: 'flex', justifyContent: 'center' }}>
-                <motion.div
-                    variants={animateModalContainer}
-                    initial='hidden'
-                    animate='visible'
-                    exit='exit'
-                    className='modal-container'>
-                    <motion.form
-                        variants={animateFormContainer}
+        <AnimatePresence exitBeforeEnter>
+            {props.showModal && <motion.div
+                variants={animateModalBackground}
+                initial='hidden'
+                animate='visible'
+                exit='exit'
+                className='modal-background'>
+                <div style={{ display: 'flex', justifyContent: 'center' }}>
+                    <motion.div
+                        variants={animateModalContainer}
                         initial='hidden'
                         animate='visible'
                         exit='exit'
-                        action="" className='modal-form-container'>
-                        <label htmlFor="title">Title</label>
-                        <input type="text" name="title" id="title" placeholder='Title' />
-                        <label htmlFor="link">Link</label>
-                        <input type="text" name="link" id="link" placeholder='Link' />
-                    </motion.form>
-                    <div className='modal-button-container'>
-                        <motion.button
-                            variants={animateButton}
+                        className='modal-container'>
+                        <motion.form
+                            variants={animateFormContainer}
                             initial='hidden'
                             animate='visible'
                             exit='exit'
-                            whileHover='onHover'
-                            style={{ backgroundColor: '#ffa69e' }}
-                            onClick={() => console.log('click')}>Cancel</motion.button>
-                        <motion.button
-                            variants={animateButton}
-                            initial='hidden'
-                            animate='visible'
-                            exit='exit'
-                            whileHover='onHover'
-                            onClick={() => console.log('click')}>Create</motion.button>
-                    </div>
-                </motion.div>
-            </div>
-        </motion.div>
+                            action="" className='modal-form-container'>
+                            <label htmlFor="title">Title</label>
+                            <input type="text" name="title" id="title" placeholder='Title' />
+                            <label htmlFor="link">Link</label>
+                            <input type="text" name="link" id="link" placeholder='Link' />
+                        </motion.form>
+                        <div className='modal-button-container'>
+                            <motion.button
+                                variants={animateButton}
+                                initial='hidden'
+                                animate='visible'
+                                exit='exit'
+                                whileHover='onHover'
+                                style={{ backgroundColor: '#ffa69e' }}
+                                onClick={() => props.onChangeShowModal(false)}>Cancel</motion.button>
+                            <motion.button
+                                variants={animateButton}
+                                initial='hidden'
+                                animate='visible'
+                                exit='exit'
+                                whileHover='onHover'
+                                onClick={() => console.log('click')}>Create</motion.button>
+                        </div>
+                    </motion.div>
+                </div>
+            </motion.div>}
+        </AnimatePresence>
     );
 }
 
