6be5a3d21944e03e790412d0ec1b943d2274ccb4
josh1506 <51785959+josh1506@users.noreply.github.com>
2021-04-15 10:43:55 +0800

    Add Redux provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { BrowserRouter } from 'react-router-dom'
+import { Provider } from 'react-redux'
 
 import App from './component/App';
+import configStore from './component/app/configStore'
 
 ReactDOM.render(
-    <BrowserRouter>
-        <App />
-    </BrowserRouter>,
+    <Provider store={configStore}>
+        <BrowserRouter>
+            <App />
+        </BrowserRouter>
+    </Provider>,
     document.getElementById('root'))
