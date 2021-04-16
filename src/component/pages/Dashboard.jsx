dbd84ac7fbbaa56d62499032327af585aeb8a505
josh1506 <51785959+josh1506@users.noreply.github.com>
2021-04-16 17:42:24 +0800

    Add graph and data

--- src/component/pages/Dashboard.jsx
+++ src/component/pages/Dashboard.jsx
@@ -1,13 +1,14 @@
 import React, { useState, useEffect } from 'react';
 import { motion } from 'framer-motion'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faTrashAlt, faPlusCircle } from '@fortawesome/free-solid-svg-icons'
+import { faTrashAlt, faPlusCircle, faMinus } from '@fortawesome/free-solid-svg-icons'
 import { AreaChart, XAxis, YAxis, CartesianGrid, Tooltip, Area } from 'recharts'
 
 import CreateUrlModal from '../common/CreateUrlModal';
-import '../assets/css/pages/dashboard.css'
 import DeleteUrlModal from './../common/DeleteUrlModal';
-import { connect } from 'react-redux';
+import route from '../route/traq'
+import '../assets/css/pages/dashboard.css'
+import { Link } from 'react-router-dom';
 
 const animateTableContainer = {
     hidden: { y: '10vh', opacity: 0 },
@@ -27,10 +28,25 @@ const animateChartList = {
     exit: { opacity: 0 }
 }
 
-function Dashboard(props) {
+function Details(props) {
+    var num = 0
     const [showCreateModal, setShowCreateModal] = useState(false)
     const [showDeleteModal, setShowDeleteModal] = useState(false)
-    const data = [
+    const [selectedUrl, setSelectedUrl] = useState({ link: "", title: "", total_visitors: 0 })
+
+    const [error, setError] = useState(false)
+    const [urlList, setUrlList] = useState([])
+    const [urlDetails, setUrlDetails] = useState({
+        "title": "",
+        "link": "",
+        "short_url": "",
+        "total_visitors": 0,
+        "viewer_list": []
+    })
+
+    const [graphList, setGraphList] = useState([])
+    const [graphTitle, setGraphTitlte] = useState([])
+    const [graphData, setGraphData] = useState([
         {
             "name": "Jan 1",
             "YouTube": 32,
@@ -43,41 +59,50 @@ function Dashboard(props) {
             "Facebook": 13,
             "amt": 2210
         },
-        {
-            "name": "Jan 3",
-            "YouTube": 18,
-            "Facebook": 52,
-            "amt": 2290
-        },
-        {
-            "name": "Jan 4",
-            "YouTube": 3,
-            "Facebook": 36,
-            "amt": 2000
-        },
-        {
-            "name": "Jan 5",
-            "YouTube": 34,
-            "Facebook": 17,
-            "amt": 2181
-        },
-        {
-            "name": "Jan 6",
-            "YouTube": 16,
-            "Facebook": 25,
-            "amt": 2500
-        },
-        {
-            "name": "Jan 7",
-            "YouTube": 25,
-            "Facebook": 23,
-            "amt": 2100
-        }
-    ]
+    ])
+    const handleGraphTitle = () => {
+        const graphTitleList = []
+        var colors = [
+            "#8884d8",
+            "#82ca9d",
+            "#e63946",
+            "#e56b6f",
+            "#555b6e",
+            "#e9c46a",
+            "#c1121f",
+            "#5a189a",
+            "#585123",
+            "#06d6a0",
+        ];
+
+        const titleList = graphData.map(data => Object.keys(data))
+        titleList.map(title => title.filter(data => data !== 'name')
+            .map(data => graphTitleList.includes(data) ? '' : graphTitleList.push(data)))
+
+        const newGraphTitle = graphTitleList.map((title, index) => { return { title, color: colors[index] } })
+        setGraphTitlte(newGraphTitle)
+    }
 
     useEffect(() => {
-        if (!props.auth) props.history.replace('/login')
-    }, [props.auth])
+        const user = localStorage.getItem('auth_token')
+
+        if (!user) props.history.replace('login')
+
+        const getData = async () => {
+            await route.get('url_tracker/url', { headers: { Authorization: `Token ${user}` } })
+                .then(({ data }) => {
+                    console.log(data)
+                    setError(false)
+                    setUrlList(data.data)
+                    setGraphList(data.graph_list)
+                    setGraphData(data.graph_list.date)
+                    handleGraphTitle()
+                })
+        }
+        getData()
+    }, [])
+
+    if (error) return <div className='dashboard-container'><p>Not found</p></div>
 
     return (
         <div className='dashboard-container'>
@@ -89,14 +114,37 @@ function Dashboard(props) {
                     exit='exit'
                     className='dashboard-status-list'
                 >
-                    <motion.p whileHover={{ scale: 1.2 }} style={{ cursor: 'default' }}>asd</motion.p>
-                    <motion.p whileHover={{ scale: 1.2 }} style={{ cursor: 'default' }}>asd</motion.p>
-                    <motion.p whileHover={{ scale: 1.2 }} style={{ cursor: 'default' }}>asd</motion.p>
-                    <motion.p whileHover={{ scale: 1.2 }} style={{ cursor: 'default' }}>asd</motion.p>
-                    <motion.p whileHover={{ scale: 1.2 }} style={{ cursor: 'default' }}>asd</motion.p>
+                    {urlList.map((url, index) => {
+                        var colors = [
+                            "#8884d8",
+                            "#82ca9d",
+                            "#e63946",
+                            "#e56b6f",
+                            "#555b6e",
+                            "#e9c46a",
+                            "#c1121f",
+                            "#5a189a",
+                            "#585123",
+                            "#06d6a0",
+                        ]
+
+                        return <motion.p whileHover={{ scale: 1.1, x: 15 }} style={{ cursor: 'default', color: colors[index] }}>
+                            <FontAwesomeIcon
+                                icon={faMinus}
+                                size='xs'
+                                color={colors[index]}
+                                style={{ marginRight: 5 }}
+                                className='table-create-icon'
+                                onClick={() => setShowCreateModal(true)}
+                            />{url.title}
+                        </motion.p>
+                    })}
                 </motion.div>
                 <div className='dashboard-graph-container'>
                     <div className="dashboard-button">
+                        <button onClick={() => setGraphData(graphList.date)}>Date</button>
+                        <button onClick={() => setGraphData(graphList.month)}>Month</button>
+                        <button onClick={() => setGraphData(graphList.year)}>Year</button>
                         <FontAwesomeIcon
                             icon={faPlusCircle}
                             size='lg'
@@ -105,24 +153,32 @@ function Dashboard(props) {
                         />
                     </div>
                     <div className='dashboard-chart'>
-                        <AreaChart width={730} height={250} data={data}
+                        <AreaChart width={730} height={250} data={graphData}
                             margin={{ top: 10, right: 30, left: 0, bottom: 0 }}>
                             <defs>
-                                <linearGradient id="colorUv" x1="0" y1="0" x2="0" y2="1">
-                                    <stop offset="5%" stopColor="#ff6b35" stopOpacity={0.8} />
-                                    <stop offset="95%" stopColor="#ff6b35" stopOpacity={0} />
-                                </linearGradient>
-                                <linearGradient id="colorPv" x1="0" y1="0" x2="0" y2="1">
-                                    <stop offset="5%" stopColor="#8884d8" stopOpacity={0.8} />
-                                    <stop offset="95%" stopColor="#8884d8" stopOpacity={0} />
-                                </linearGradient>
+                                {graphTitle.map(graph => {
+                                    num += 1
+                                    return <linearGradient id={graph.title} x1="0" y1="0" x2="0" y2="1">
+                                        <stop offset="5%" stopColor={graph.color} stopOpacity={0.8} />
+                                        <stop offset="95%" stopColor={graph.color} stopOpacity={0} />
+                                    </linearGradient>
+                                })}
                             </defs>
                             <XAxis dataKey="name" />
                             <YAxis />
                             <CartesianGrid strokeDasharray="3 3" />
                             <Tooltip />
-                            <Area type="monotone" dataKey="YouTube" stroke="#ff6b35" fillOpacity={1} fill="url(#colorUv)" />
-                            <Area type="monotone" dataKey="Facebook" stroke="#8884d8" fillOpacity={1} fill="url(#colorPv)" />
+                            {graphTitle.map(graph => {
+                                num += 1
+                                return <Area
+                                    key={`${graph.title}${num}`}
+                                    type="monotone"
+                                    dataKey={graph.title}
+                                    stroke={graph.color}
+                                    fillOpacity={1}
+                                    fill={`url(#${graph.title})`}
+                                />
+                            })}
                         </AreaChart>
                     </div>
                 </div>
@@ -133,9 +189,8 @@ function Dashboard(props) {
                 animate='visible'
                 exit='exit'
                 type="text"
-                name="" id=""
                 className='search-input'
-                placeholder='Input link title'
+                placeholder='Input Date or Month'
             />
             <motion.div
                 variants={animateTableContainer}
@@ -149,41 +204,53 @@ function Dashboard(props) {
                         <tr>
                             <th>Title</th>
                             <th>Link</th>
+                            <th>Track Link</th>
                             <th>Total Visitors</th>
                             <th>Last Viewed</th>
                             <th style={{ minWidth: '15px' }}></th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr>
-                            <td>ASDASDASDASDASDASDASDASDASD</td>
-                            <td>asdasdasdasdasdasdasdasdasdasdasdasdasdasd</td>
-                            <td>123</td>
-                            <td>asd</td>
-                            <td>
-                                <FontAwesomeIcon icon={faTrashAlt} size='lg' className='table-delete-icon' onClick={() => setShowDeleteModal(true)} />
-                            </td>
-                        </tr>
-                        <tr>
-                            <td>ASD</td>
-                            <td>asd</td>
-                            <td>123</td>
-                            <td>asd</td>
-                            <td>
-                                <FontAwesomeIcon icon={faTrashAlt} size='lg' className='table-delete-icon' onClick={() => setShowDeleteModal(true)} />
-                            </td>
-                        </tr>
+                        {urlList.map(url => url.title &&
+                            <tr key={url.title}>
+                                <td>
+                                    <Link to={`/dashboard/${url.id}`}>
+                                        {url.title.substring(0, 8)}
+                                        {url.title.length > 8 ? '...' : ''}
+                                    </Link>
+                                </td>
+                                <td>
+                                    <a href={url.link} target='_blank'>
+                                        Visit page
+                                        </a>
+                                </td>
+                                <td>http://localhost:3000/url/{url.short_url}</td>
+                                <td>{url.total_visitors}</td>
+                                <td>on going</td>
+                                <td>
+                                    <FontAwesomeIcon
+                                        className='table-delete-icon'
+                                        size='lg'
+                                        icon={faTrashAlt}
+                                        onClick={() => {
+                                            setSelectedUrl(url)
+                                            setShowDeleteModal(true)
+                                        }} />
+                                </td>
+                            </tr>
+                        )}
                     </tbody>
                 </table>
             </motion.div>
             <CreateUrlModal showModal={showCreateModal} onChangeShowModal={setShowCreateModal} />
-            <DeleteUrlModal showModal={showDeleteModal} onChangeShowModal={setShowDeleteModal} />
+            <DeleteUrlModal
+                showModal={showDeleteModal}
+                onChangeShowModal={setShowDeleteModal}
+                onSelectItem={selectedUrl}
+                onChangeSelect={setSelectedUrl}
+            />
         </div>
     );
 }
 
-const mapStateToProps = ({ auth }) => {
-    return { auth }
-}
-
-export default connect(mapStateToProps)(Dashboard);
+export default Details;
