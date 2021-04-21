979a26d1d30dc751230a30bd3e548eeb54d10faa
josh1506 <51785959+josh1506@users.noreply.github.com>
2021-04-21 20:18:16 +0800

    Update App.jsx

--- src/component/App.jsx
+++ src/component/App.jsx
@@ -1,15 +1,16 @@
 import React, { useEffect } from 'react';
-import { Switch, Route } from 'react-router-dom'
+import { Switch, Route, Redirect } from 'react-router-dom'
 import { AnimatePresence } from 'framer-motion'
 import { connect } from 'react-redux'
 
 import Dashboard from './pages/Dashboard';
 import Details from './pages/Details';
+import Footer from './common/Footer';
 import Landing from './pages/Landing';
 import Login from './pages/Login';
 import Nav from './common/Nav';
-import Footer from './common/Footer';
-import Viewer from './pages/Viewer';
+import NotFound from './pages/NotFound';
+import route from './route/traq'
 import { getCurrentUser } from './app/auth'
 import './assets/css/app.css'
 
@@ -17,6 +18,12 @@ function App(props) {
     useEffect(() => {
         props.getCurrentUser()
     }, [])
+
+    const handleViewer = async({match, history}) => {
+        await route.get(`url_tracker/view/${match.params.shortUrl}`)
+        .then(({data}) => window.location.replace(data.data))
+        .catch(() => history.push('/not-found'))
+    }
 
     return (
         <div>
@@ -27,11 +34,16 @@ function App(props) {
                     render={({ location }) => (
                         <AnimatePresence exitBeforeEnter>
                             <Switch location={location} key={location.key}>
-                                <Route path='/url/:shortUrl' component={Viewer} />
+                                <Route path='/url/:shortUrl' component={props =>{
+                                    handleViewer(props)
+                                return null
+                                }} />
                                 <Route path='/login' component={Login} />
                                 <Route path='/dashboard/:id' component={Details} />
                                 <Route path='/dashboard' component={Dashboard} />
-                                <Route path='/' component={Landing} />
+                                <Route path='/not-found' component={NotFound} />
+                                <Route path='/' exact component={Landing} />
+                                <Redirect to='/not-found' />
                             </Switch>
                         </AnimatePresence>
                     )}
