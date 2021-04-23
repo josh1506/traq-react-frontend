f88ac93753f195fb15581ced4590b1502315a679
josh1506 <51785959+josh1506@users.noreply.github.com>
2021-04-23 19:53:41 +0800

    updated links

--- src/component/pages/Details.jsx
+++ src/component/pages/Details.jsx
@@ -156,7 +156,7 @@ function Details(props) {
                     </p>
                     <p>Trach url: 
                         <p style={{color: '#8884d8'}}>
-                        https://itraq.netlify.app/url/{urlDetails.short_url}
+                        https://josh1506.github.io/traq-react-frontend/url/{urlDetails.short_url}
                         </p>
                     </p>
                     <p>Number of Visitors: 
